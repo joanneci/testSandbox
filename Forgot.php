6ccb883294444c252dabbd7d40321b74d2f4d950
joanneci <joanneci@optonline.net>
2014-05-20 19:25:56 +0000

    big hook test

--- Forgot.php
+++ Forgot.php
@@ -9,9 +9,18 @@
 class ForgotController extends Controller {
     public function run () {
         // @TODO: We extra the request but it seems we then don't use it?
+        
+    
+    
+    
+    
+    
+    	//this is a tab
+    //this has trailing whitespaces             
+//this is a very long line kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk lllllllllllllllllllllllllllllllllllllllllllllllll bbbbbbbbbbbbbbbbbbbbbbbbbbbbb 
         extract($_REQUEST);
 
-        $msg = '';
+        $msg = '';                          
         if(!empty($_POST['username'])) {                     
             
             $token = md5(uniqid());
@@ -21,7 +30,7 @@ class ForgotController extends Controller {
                 $user->save();
                 $resetUrl = SECURE_SERVER_URL . 'resetpass?un=' . base64_encode($_POST['username']) . '&amp;token=' . $token;
                 $resetUrl = '<a href="' . $resetUrl . '" title="Password Recovery">' . $resetUrl . '</a>';
-                sendTemplateEmail($_POST['username'], 'recovery', array('url' => $resetUrl));
+                sendTemplateEmail($_POST['username'], 'recovery', array('url' => $resetUrl));                          
                 $msg = '<p class="LV_valid">Login information will be sent if the email address ' . $_POST['username'] . ' is registered.</p>';
             } else {
                 $msg = '<p class="LV_invalid">Sorry, unable to send password reset information. Try again or contact an administrator.</p>';
