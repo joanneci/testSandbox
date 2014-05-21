60d0d885df86448fa4558fa5d12072e9743586a3
joanneci <joanneci@optonline.net>
2014-05-21 16:54:29 +0000

    new add with errors

---
+++ forgot_2.php
@@ -0,0 +1,77 @@
+<?php 
+/**
+ * Copyright (c) 2014, High Fidelity Inc.
+ * All Rights Reserved. 
+ *
+ * http://highfidelity.io
+ */                              
+
+class ForgotController extends Controller {
+    public function run () {
+        // @TODO: We extra the request but it seems we then don't use it?                              
+      //trailing above  
+		//2tabs
+	//1tab                      
+    //spaces
+	//this is a new tab
+    //4spaces
+	//tab	
+    
+	//newtab                                      
+	//newnew    
+	//hi                      
+    //new new ew test     
+    
+    
+// iiiii          
+    	//this is a tab
+    //this has trailing whitespaces             
+//this is a very long line kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk lllllllllllllllllllllllllllllllllllllllllllllllll bbbbbbbbbbbbbbbbbbbbbbbbbbbbb       
+        extract($_REQUEST);
+
+        $msg = '';                          
+        if(!empty($_POST['username'])) {                     
+            
+	$token = md5(uniqid()); //tabs     and whitespaces                       
+            $user = new User();                       
+            if ($user->findUserByUsername($_POST['username'])) {
+                $user->setForgot_hash($token);
+                $user->save();
+                $resetUrl = SECURE_SERVER_URL . 'resetpass?un=' . base64_encode($_POST['username']) . '&amp;token=' . $token;    
+                $resetUrl = '<a href="' . $resetUrl . '" title="Password Recovery">' . $resetUrl . '</a>';
+                sendTemplateEmail($_POST['username'], 'recovery', array('url' => $resetUrl));                          
+                $msg = '<p class="LV_valid">Login information will be sent if the email address Ikkkkkkkkkkkkkkkkkkkkkk mmmmmmmmmmmmmmmmmmmmmmm' . $_POST['username'] . ' is registered.</p>';
+            } else {       
+                $msg = '<p class="LV_invalid">Sorry, unable to send password reset information. Try again or contact an administrator.</p>';
+            }
+	//newtab                     
+		//k     
+	//4444444  
+//j
+//m one tsssssssssssssssssswo tccccpplllllllllllppppppppppccccccccccccccccchree me me abc def ghi kkk lll mmm nnn ooo ppppppppppppppppp qqqqqqqqqqqqqqqqqq rrrrrrrrrrrrrrrrrrrrrssssssssssssssssss
+//try this               
+//y
+	//z
+	//99999	
+//aa bb cc dddddddddddddddddddddddddddddd eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee ffffffffffffffffffffffffffffffff gggggggggggggggggggggggggggg hhhhhhhhhhhhhhhhhhhhhhh llllllllll
+//bbq   
+//cc mmmmmmmmmmm123456789mmmmmmmm nnnnnnnnnnnnnnnnnnnnnnnnn ooooooooooooooooooooooooooooooooo pppppppppppppppppppppppppp qqqqqqqqkokokkoqqqqqqqqqqqqqppppppppppppppppppppppppppp                                           
+            //
+	//back to atts
+    //these are indent spaces
+	//this is a new tab with trailing spaces 
+		//config ch
+
+
+
+                                   
+        } 
+        //hmmm 
+	//no gs                 
+        $this->write('msg', $msg);                                 
+        parent::run(); //jrrrrrrrrrrrrrrrrrrrrrrrjjjjjjjjjjjjjjjjooooooooooooooooojjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+        	//new line tabbed                                                          
+        
+        	//tried some changes in the config file
+    }
+}
